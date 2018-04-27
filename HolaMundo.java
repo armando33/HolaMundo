07f2cbfeaaaef3dd6ccee258d6a5ceb77283143a
Armando Canaza Ayarachi <dotoarm@gmail.com>
2018-04-27 01:26:00 -0400

    Añadiendo HolaMundo y el .gitignore

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+/**
+* Clase de con java
+* @autor: Armando Canaza
+* @since 1.0.0
+*/
+public class HolaMundo{
+    /**
+     * Metodo Estatico que ejecuta el programa
+     */
+    public static void main (String[] args){
+        System.out.println("Hola Mundo");
+    }
+}
